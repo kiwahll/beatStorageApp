7f147b9fabf6981b340b587efa4f462344b53dad
kiwahll <laurenzbiller7@gmail.com>
2025-05-06 21:23:41 +0200

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::io::{self, stdout};
-use crossterm::{cursor::MoveTo, execute, terminal::{Clear, ClearType}};
+use crossterm::{
+    cursor::MoveTo,
+    execute,
+    terminal::{Clear, ClearType},
+};
 use dialoguer::Select;
 use serde_json::Value;
-use thiserror::Error;
+use std::process::Command;
+use std::{
+    io::{self, stdout},
+    process::exit,
+};
 
 #[tokio::main]
 async fn main() {
+    clear();
     let json: Value = fetch_data().await.unwrap();
 
     let mut index: usize = 0;
     loop {
-        index = menu(&json, index.clone()).into();
+        index = menu(&json, index.clone()).unwrap_or_else(|e| {
+            eprintln!("{}", e);
+            pause();
+            exit(1);
+        });
         let item: &&Value = &json.as_array().unwrap().get(index).unwrap();
 
         for key in item.as_object().unwrap().keys() {
@@ -19,21 +31,46 @@ async fn main() {
 
         // PAUSE
         _ = io::stdin().read_line(&mut String::new());
-        // Clear
-        execute!(
-            stdout(),
-            Clear(ClearType::All),
-            MoveTo(0, 0)
-        ).unwrap()
+
+        let status = Command::new("yt-dlp")
+            .args([
+                "-f",
+                "bestaudio",
+                "-o",
+                "output.%(ext)s",
+                "--extract-audio",
+                "--audio-format",
+                "flac",
+                item.get("url").unwrap().as_str().unwrap(),
+            ])
+            .status()
+            .expect("failed to execute yt-dlp");
+
+        println!("Status: {}", status);
+
+        pause();
+        clear();
     }
 }
 
-fn menu(json: &Value, index: usize) -> usize {
+fn clear() {
+    execute!(stdout(), Clear(ClearType::All), MoveTo(0, 0)).unwrap();
+}
+
+fn pause() {
+    _ = io::stdin().read_line(&mut String::new());
+}
+
+fn menu(json: &Value, index: usize) -> Result<usize, Box<dyn std::error::Error>> {
     let mut selection_items: Vec<&str> = Vec::new();
     if let Some(array) = json.as_array() {
         for item in array {
             selection_items.push(item.get("title").unwrap().as_str().unwrap());
         }
+    }
+
+    if *&selection_items.is_empty() {
+        return Err("No items found in JSON".into());
     }
 
     let selection = Select::new()
@@ -42,21 +79,22 @@ fn menu(json: &Value, index: usize) -> usize {
         .interact()
         .unwrap();
 
-    selection
+    Ok(selection)
 }
 
-async fn fetch_data() -> Result<Value, MyError> {
-    let url: &str = "https://api.sampleapis.com/coffee/hot";
-    let response = reqwest::get(url).await?.text().await?;
-    let json: Value = serde_json::from_str(&response)?;
+async fn fetch_data() -> Result<Value, Box<dyn std::error::Error>> {
+    let url: &str = "http://127.0.0.1:8000";
+    let response = reqwest::get(url)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("{}", e);
+            pause();
+            exit(1);
+        })
+        .text()
+        .await
+        .unwrap();
+    let json: Value = serde_json::from_str(&response).unwrap();
+
     Ok(json)
-}
-
-#[derive(Error, Debug)]
-pub enum MyError {
-    #[error("Network error: {0}")]
-    Request(#[from] reqwest::Error),
-
-    #[error("JSON error: {0}")]
-    Json(#[from] serde_json::Error)
 }
